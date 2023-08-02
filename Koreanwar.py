5888a579e2b5c5ad1426bd5a91e4c19bfff81838
kimminam <bluemind8520@gmail.com>
2023-08-02 15:36:20 +0900

    z

---
+++ Koreanwar.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://encykorea.aks.ac.kr/Article/E0042143"
+
+res = requests.get(url)
+res.raise_for_status()
+soup = BeautifulSoup(res.text,"html")
+title = soup.find(attrs={"id":"section-4"})
+content = title.next_sibling
+print(res.raise_for_status())
