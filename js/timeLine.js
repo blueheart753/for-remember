87a5ed46fb5277bd6dde4427e43d7f2e95b3def6
kimminam <bluemind8520@gmail.com>
2023-08-04 02:04:25 +0900

    fiix link

--- js/timeLine.js
+++ js/timeLine.js
@@ -2,17 +2,17 @@ const wars = [
   {
     pic: "<img src='/src/war_first.png'>",
     description:
-      "<p><strong> <span>사전에 충분히 계획되고 막강한 군사력을 지닌 북한군이 초전에 우세</span></strong>하였던 것은 당연하다. 전쟁을 일으킨 다음날인 6월 26일에 <strong><span>북한의 김일성은 이 전쟁을 가리켜 남한을 ‘해방’시켜 ‘조선민주주의 인민공화국’으로 하여금 조국통일을 성취하기 위한 전쟁</span></strong>이라고 방송하였다. 정치가 불안정한 가운데 무방비상태에 있던 한국에 대한 북한의 기습공격은 6월 27일에는 이미 서울을 점령하기에 이르렀고, 7월 3일에는 한강을 넘어 파죽지세로 남진을 계속하였다. 한편, 일본에 주둔하고 있던 미 제24보병사단이 즉시 한국으로 이동하여 적의 진격을 저지하려 하였으나 전세를 만회하기에는 역부족이었다. 그리하여 <strong><span>유엔군은 부산을 거점으로 한 낙동강 방어선을 확보하여 항쟁하면서 반격의 기회를 노렸다.</span></strong> 한편, 점령지역에서의 북한의 정책은 그들이 내세운 ‘해방’정책과는 정면으로 상반되는 것이었다.</p>",
+      '<p><strong> <span>사전에 충분히 계획되고 막강한 군사력을 지닌 북한군이 초전에 우세</span></strong>하였던 것은 당연하다. 전쟁을 일으킨 다음날인 6월 26일에 <strong><span>북한의 김일성은 이 전쟁을 가리켜 남한을 ‘해방’시켜 ‘조선민주주의 인민공화국’으로 하여금 조국통일을 성취하기 위한 전쟁</span></strong>이라고 방송하였다. 정치가 불안정한 가운데 무방비상태에 있던 한국에 대한 북한의 기습공격은 6월 27일에는 이미 서울을 점령하기에 이르렀고, 7월 3일에는 한강을 넘어 파죽지세로 남진을 계속하였다. 한편, 일본에 주둔하고 있던 미 제24보병사단이 즉시 한국으로 이동하여 적의 진격을 저지하려 하였으나 전세를 만회하기에는 역부족이었다. 그리하여 <strong><span>유엔군은 부산을 거점으로 한 낙동강 방어선을 확보하여 항쟁하면서 반격의 기회를 노렸다.</span></strong> 한편, 점령지역에서의 북한의 정책은 그들이 내세운 ‘해방’정책과는 정면으로 상반되는 것이었다.</p>',
   },
   {
     pic: "<img src='/src/war_second.jpg'>",
     description:
-      "<p>북한군에 치명적인 일격을 가하기 위한 인천상륙작전 계획이 구상되기 시작한 것은 전쟁이 발발하고 나서 불과 얼마 되지 않은 7월 15일부터였다.<br>낙동강 전선에서 총반격을 시작한 것과 때를 같이하여 미 제1해병사단과 제7사단으로 이루어진 제10군단 및 5,000명에 달하는 한국해병대는 드디어 <strong>9월 15일 새벽에 인천 월미도에 기습상륙하고 그 다음날에는 인천을 함락시켰다.</strong> 미 해병대와 한국군은 서울 탈환을 목적으로 동진하였고, 미 제7사단은 남진하여 북상하는 유엔군과 오산에서 합류함으로써 북한군은 남북으로 단절되었고, 한반도의 중부 및 동부산악지대로 패주하였다.<br>인천에 상륙한 유엔군은 9월 26일에는 서울에 진입하였고, 완전히 회복한 뒤인 9월 29일에는 서울수복 기념식이 거행되었다. 수도 서울을 탈환한 유엔군은 동해안과 서해안을 따라 38선에 가깝게 계속 북상하였다. 인천상륙작전의 성공을 계기로 전세는 완전히 뒤집혔다.</p>",
+      '<p>북한군에 치명적인 일격을 가하기 위한 인천상륙작전 계획이 구상되기 시작한 것은 전쟁이 발발하고 나서 불과 얼마 되지 않은 7월 15일부터였다.<br>낙동강 전선에서 총반격을 시작한 것과 때를 같이하여 미 제1해병사단과 제7사단으로 이루어진 제10군단 및 5,000명에 달하는 한국해병대는 드디어 <strong>9월 15일 새벽에 인천 월미도에 기습상륙하고 그 다음날에는 인천을 함락시켰다.</strong> 미 해병대와 한국군은 서울 탈환을 목적으로 동진하였고, 미 제7사단은 남진하여 북상하는 유엔군과 오산에서 합류함으로써 북한군은 남북으로 단절되었고, 한반도의 중부 및 동부산악지대로 패주하였다.<br>인천에 상륙한 유엔군은 9월 26일에는 서울에 진입하였고, 완전히 회복한 뒤인 9월 29일에는 서울수복 기념식이 거행되었다. 수도 서울을 탈환한 유엔군은 동해안과 서해안을 따라 38선에 가깝게 계속 북상하였다. 인천상륙작전의 성공을 계기로 전세는 완전히 뒤집혔다.</p>',
   },
   {
     pic: "<img src='/src/war_third.jpg'>",
     description:
-      "<p><strong>10월 24일에 드디어 한국군 제6사단이 청천강 상류에 있는 운산에서 중공군으로 보이는 적군에 의하여 포위</strong>되었고, 이를 구원하기 위한 미 제1기병사단도 그 달 26일에 포위당해 고전하였다. 중공군의 참전이 공식적으로 확인되지 않은 상태에서 유엔군은 보급에 곤란을 겪었고, 미군은 청천강 남쪽까지의 후퇴를 명하였다.<br>중공군의 개입으로 전세는 역전이 되고 장기화할 조짐이 나타났다. <strong><span>중공군의 원조에 힘입은 북한군은 12월 26일에는 다시 38선을 넘어 남진하였다.</span></strong> 맥아더는 미국에 지상 증원군을 요청하고 새로운 정치적 결정과 전략계획을 제시하였다. 패전의 와중에서 맥아더는 중공이 동조한다면 38선에 따라 휴전하는 것도 지지한다고 하면서도 중공에 대한 전면적 대응을 주장하였다. 즉, 미국의 해 · 공군으로 중국 해안을 봉쇄하고 만주 폭격, 그리고 대만에 있는 국부군을 한국전에 투입하는 한편, 중국의 남부지방에 상륙시켜 제2의 전선을 펴자는 것이었다.</p>",
+      '<p><strong>10월 24일에 드디어 한국군 제6사단이 청천강 상류에 있는 운산에서 중공군으로 보이는 적군에 의하여 포위</strong>되었고, 이를 구원하기 위한 미 제1기병사단도 그 달 26일에 포위당해 고전하였다. 중공군의 참전이 공식적으로 확인되지 않은 상태에서 유엔군은 보급에 곤란을 겪었고, 미군은 청천강 남쪽까지의 후퇴를 명하였다.<br>중공군의 개입으로 전세는 역전이 되고 장기화할 조짐이 나타났다. <strong><span>중공군의 원조에 힘입은 북한군은 12월 26일에는 다시 38선을 넘어 남진하였다.</span></strong> 맥아더는 미국에 지상 증원군을 요청하고 새로운 정치적 결정과 전략계획을 제시하였다. 패전의 와중에서 맥아더는 중공이 동조한다면 38선에 따라 휴전하는 것도 지지한다고 하면서도 중공에 대한 전면적 대응을 주장하였다. 즉, 미국의 해 · 공군으로 중국 해안을 봉쇄하고 만주 폭격, 그리고 대만에 있는 국부군을 한국전에 투입하는 한편, 중국의 남부지방에 상륙시켜 제2의 전선을 펴자는 것이었다.</p>',
   },
   {
     pic: "<img src='/src/war_fourth.jpg'>",
@@ -24,17 +24,18 @@ const wars = [
     ③ 휴전협정 성립 후에 개최될 한국의 정치적 통일에 대하여 90일 간 아무런 구체적인 성과가 없을 때 미국은 그 회의에서 탈퇴한다.
     <br>
     ④ 한국군의 증강을 위한 미국의 원조 약속 등이다.
-    </span></strong></p>`,
+    </span></strong><br>
+    이로써 1953년 7월 27일에 비로소 휴전협정이 이루어짐으로써 3년 1개월에 걸친 전쟁은 중지되고 휴전이 성립되었다.</p>`,
   },
 ];
 
-const first_war = document.getElementById("radio1");
-const second_war = document.getElementById("radio2");
-const third_war = document.getElementById("radio3");
-const fourth_war = document.getElementById("radio4");
+const first_war = document.getElementById('radio1');
+const second_war = document.getElementById('radio2');
+const third_war = document.getElementById('radio3');
+const fourth_war = document.getElementById('radio4');
 
-const image_div = document.getElementById("imgdiv");
-const result = document.getElementById("description");
+const image_div = document.getElementById('imgdiv');
+const result = document.getElementById('description');
 
 function addpic(pic) {
   image_div.innerHTML = pic;
@@ -49,18 +50,18 @@ function handleWarSelection(index) {
   addresult(wars[index].description);
 }
 
-first_war.addEventListener("click", () => {
+first_war.addEventListener('click', () => {
   handleWarSelection(0);
 });
 
-second_war.addEventListener("click", () => {
+second_war.addEventListener('click', () => {
   handleWarSelection(1);
 });
 
-third_war.addEventListener("click", () => {
+third_war.addEventListener('click', () => {
   handleWarSelection(2);
 });
 
-fourth_war.addEventListener("click", () => {
+fourth_war.addEventListener('click', () => {
   handleWarSelection(3);
 });
