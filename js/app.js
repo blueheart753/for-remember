42be001c55b6e91a469bc54f514b43069477db6b
kimminam <bluemind8520@gmail.com>
2024-04-18 07:27:37 +0900

    줄정렬

--- js/app.js
+++ js/app.js
@@ -1,40 +1,36 @@
-const footers = document.querySelector(".foot");
+const footers = document.querySelector('.foot');
 
-const Ptag = document.createElement("p");
+const Ptag = document.createElement('p');
 
 footers.append(Ptag);
 
-Ptag.className = "about";
+Ptag.className = 'about';
 
 const copyRight = `Photo Designed by Freepik`;
 
 Ptag.append(copyRight);
 
 let searchInput;
-searchInput = document.querySelector("#input");
+searchInput = document.querySelector('#input');
 
-searchInput.addEventListener("keydown", (e) => {
+searchInput.addEventListener('keydown', e => {
   if (e.keyCode === 13) {
     switch (searchInput.value) {
-      case "war":
-        location.href = "war.html";
+      case 'war':
+        location.href = 'war.html';
         break;
-
-      case "veterans":
-        location.href = "veterans.html";
+      case 'timeLine':
+        location.href = 'timeline.html';
         break;
-      case "timeLine":
-        location.href = "timeline.html";
+      case 'warDocument':
+        location.href = 'warDocument.html';
         break;
-      case "warDocument":
-        location.href = "warDocument.html";
-        break;
-      case "AboutUs":
-        location.href = "AboutUs.html";
+      case 'AboutUs':
+        location.href = 'AboutUs.html';
         break;
 
       default:
-        alert("검색어를 입력 또는 제대로 입력해주세요");
+        alert('검색어를 입력 또는 제대로 입력해주세요');
         break;
     }
   }
